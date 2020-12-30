6dad08f38e5a5d38c5efbec54743951819a37e12
gobltpqls99 <gobltpqls99@naver.com>
2020-12-30 18:22:14 +0900

    modal하자

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
+$(".w3s").mouseenter(function(){
+	$(".visit").show();
+});
+$(".w3s").mouseleave(function(){
+	$(".visit").hide();
+});
+
+$(".blog-text h2").click(function(){
+	$(this).removeClass("on");
+	$(this).addClass("on").siblings().removeClass("on");
+	$(this).next("ul").addClass("on");
+});
+
+$(".main-round").click(function(){
+	$(this).next("ul.name-p").slideToggle();
+});
 
 
+ 
 var swiper = new Swiper('.swiper-container', {
 	loop: true,
 	navigation: {
